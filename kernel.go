e0b2dedbbda11f7c88391c368bbe53fd021937de
WardonNE <1730314864@qq.com>
2025-01-09 17:33:59 +0800

    update

--- kernel.go
+++ kernel.go
@@ -3,19 +3,42 @@ package router
 import (
 	"net/http"
 
+	"github.com/gopi-frame/contract"
+
 	"github.com/gopi-frame/contract/router"
 )
 
 type Kernel struct {
-	http.Server
+	*http.Server
 }
 
-func NewKernel(r router.Router) *Kernel {
-	return &Kernel{
-		http.Server{
-			Handler: r,
-		},
+type Option = contract.Option[*http.Server]
+
+type OptionFunc func(srv *http.Server) error
+
+func (f OptionFunc) Apply(srv *http.Server) error {
+	return f(srv)
+}
+
+func WithAddr(addr string) Option {
+	return OptionFunc(func(srv *http.Server) error {
+		srv.Addr = addr
+		return nil
+	})
+}
+
+func NewKernel(r router.Router, opts ...Option) (*Kernel, error) {
+	srv := &http.Server{
+		Handler: r,
 	}
+	for _, opt := range opts {
+		if err := opt.Apply(srv); err != nil {
+			return nil, err
+		}
+	}
+	return &Kernel{
+		Server: srv,
+	}, nil
 }
 
 func (k *Kernel) Run() error {
